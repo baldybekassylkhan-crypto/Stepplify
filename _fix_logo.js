78f63dc7f530684437f7ac0d6a2bbec498c95c48
bassylkhan <baldybekassylkhan@gmail.com>
2026-09-13 18:48:06 +0500

    Fix header logo transparent background

---
+++ _fix_logo.js
@@ -0,0 +1,1 @@
+const fs = require('fs'); ['index.html', 'catalog.html', 'map.html', '404.html'].forEach(file => { if (fs.existsSync(file)) { let content = fs.readFileSync(file, 'utf8'); content = content.replace(/<img src="assets\/logo-icon-red\.png"/g, '<img src="assets/logo-icon.png"'); fs.writeFileSync(file, content); } });
