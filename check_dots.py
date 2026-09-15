c23f25263437f4ec507d02d6d3d3583336c2bf81
bassylkhan <baldybekassylkhan@gmail.com>
2026-09-15 07:29:52 +0500

    Cleanup: remove temp helper scripts

--- check_dots.py
+++
@@ -1,7 +0,0 @@
-import re
-with open('index.html', encoding='utf-8') as f:
-    html = f.read()
-
-dots = re.findall(r'<span class="arp-dot">.*?</span>', html, flags=re.DOTALL)
-for i, d in enumerate(dots):
-    print(f'Dot {i}:', d.encode('unicode_escape').decode('ascii'))
